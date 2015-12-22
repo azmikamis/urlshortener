69bcffba9f2bb831b3ffcefc7124056d71985f35
Azmi Kamis <azmikamis@outlook.com>
2015-12-22 12:47:44 +0800

    simple shortener

--- main.go
+++ main.go
@@ -3,16 +3,36 @@ package main
 import (
   "net/http"
   "log"
+  "encoding/json"
+  "strings"
 )
+
 func main() {
 
+  var shortenedurls = make(map[int]string)
+  count := 0
+  
   http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
 
-    log.Println(r.Body)
+    var message map[string] interface{} // empty interface
+
+    if err := json.NewDecoder(r.Body).Decode(&message); err == nil {
+    
+      for key := range message {
+        if key == "url" {
+            
+          s := message["url"].(string)
+
+          count += 1
+          shortenedurls[count] = s
+
+        }
+      }
+    }
 
   })
 
-  http.HandleFunc("/original", func(w http.ResponseWriter, r *http.Request) {
+  http.HandleFunc("/expand", func(w http.ResponseWriter, r *http.Request) {
 
     log.Println(r.Body)
 
