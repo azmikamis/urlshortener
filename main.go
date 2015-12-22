525c3cc1b86445cb778192ff2555dfba1c726a2e
Azmi Kamis <azmikamis@outlook.com>
2015-12-22 13:54:55 +0800

    Add mutex and write response

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 
   var shortenedurls = make(map[int]string)
+  var mutex = &sync.Mutex{}
   count := 0
   
   http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +24,17 @@ func main() {
             
           s := message["url"].(string)
 
+          mutex.Lock()
           count += 1
           shortenedurls[count] = s
+          i := strconv.Itoa(count)
+          mutex.Unlock()
+          
+          var buffer bytes.Buffer
+          buffer.WriteString("http://localhost/")
+          buffer.WriteString(i)
+          response, _ := json.Marshal(map[string]string{"short": buffer.String()})
+          w.Write([]byte(response))
 
         }
       }
