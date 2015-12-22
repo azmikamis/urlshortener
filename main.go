b5e7a914c278a3f2c77764423c6f15923898e747
Azmi Kamis <azmikamis@outlook.com>
2015-12-22 14:21:17 +0800

    add imports, implement expand

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
   "log"
   "encoding/json"
   "strings"
+  "strconv"
+  "sync"
+  "bytes"
 )
 
 func main() {
@@ -44,10 +47,26 @@ func main() {
 
   http.HandleFunc("/expand", func(w http.ResponseWriter, r *http.Request) {
 
-    log.Println(r.Body)
+    var message map[string] interface{} // empty interface
 
+    if err := json.NewDecoder(r.Body).Decode(&message); err == nil {
+    
+      for key := range message {
+        if key == "short" {
+            
+          s := strings.Replace(message["short"].(string),"http://localhost/","", -1)
+          i, _ := strconv.Atoi(s)
+          
+          var buffer bytes.Buffer
+          buffer.WriteString(shortenedurls[i])
+          response, _ := json.Marshal(map[string]string{"expand": buffer.String()})
+          w.Write([]byte(response))
+        }
+      }
+    }
+    
   })
 
-  http.ListenAndServe(":8080", nil)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
